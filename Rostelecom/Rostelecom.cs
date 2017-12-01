e0314892cb418b5df225675e1af863ac6de05874
varinskyv <varinskyv@gmail.com>
2017-12-01 18:20:31 +0500

    Получение номера абонента и добавление его в локальную базу

--- Rostelecom/Rostelecom.cs
+++ Rostelecom/Rostelecom.cs
@@ -22,11 +22,18 @@ namespace Rostelecom
 
         private Thread classThread;
 
+        private LocalBase lBase;
+
         private static string Period_Tag = "за период с";
         private static string Subscriber_Start_Tag = "абонент Тел";
         private static string Subscriber_End_Tag = "Всего по абоненту";
         private static string Internet_Tag = "Передача данных";
         private static string End_Unit_Tag = "Передача данных";
+
+        public Rostelecom()
+        {
+            lBase = LocalBase.getInstance();
+        }
 
         //Асинхронный метод, передающий результат в вызывающий класс
         private CallBackResult CallBack(CallBackResult result)
@@ -47,11 +54,27 @@ namespace Rostelecom
             CallBackResult result = new CallBackResult();
 
             //try
-            //{
+            {
                 List<List<object>> data = (List<List<object>>)obj;
 
                 result.StartPeriodDate = GetStartPeriodDate(data);
-            //}
+
+                int i = 0;
+                while(!((string)data[i][0]).Equals("Sheet2") && i < data.Count)
+                {
+                    if (data[i].Find(cell => Convert.ToString(cell).Contains(Subscriber_Start_Tag)) != null)
+                    {
+                        Subscriber subscriber = new Subscriber();
+                        subscriber.Number = GetSubscriberNumber(data[i]);
+
+                        int Id = lBase.AddSubscriber(subscriber);
+                    }
+
+                    i++;
+                }
+
+                result.Result = true;
+            }
             //catch(Exception e)
             //{
             //    Console.WriteLine(e);
@@ -64,7 +87,7 @@ namespace Rostelecom
         {
             List<object> periodRow = data.Find(item => item.Find(row => Convert.ToString(row).Contains(Period_Tag)) != null);
 
-            string periodStr = (string)periodRow.Find(item => Convert.ToString(item).Contains(Period_Tag));
+            string periodStr = GetString(periodRow, Period_Tag);
 
             string startDate = "";
             int i = periodStr.IndexOf(Period_Tag) + Period_Tag.Length + 1;
@@ -76,6 +99,30 @@ namespace Rostelecom
             }
 
             return DateTime.Parse(startDate);
+        }
+
+        private string GetString(List<object> list, string tag)
+        {
+            return (string)list.Find(item => Convert.ToString(item).Contains(tag));
+        }
+
+        private string GetSubscriberNumber(List<object> row)
+        {
+            string result = "";
+
+            string str = GetString(row, Subscriber_Start_Tag);
+            int i = str.IndexOf(Subscriber_Start_Tag) + Subscriber_Start_Tag.Length + 1;
+            while (i < str.Length)
+            {
+                if (Char.IsDigit(str[i]))
+                {
+                    result += str[i];
+                }
+
+                i++;
+            }
+
+            return result; 
         }
 
         public void Dispose()
